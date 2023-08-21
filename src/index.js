77d21b1526376bbdf8e990a4715e5df042f20400
Cedric <116574782+TimoTimo78@users.noreply.github.com>
2023-08-21 10:26:16 +0200

    add front

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from  "./lib/redux/reducers";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import FiltersProvider from "./context";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:2021/graphql",
+  cache: new InMemoryCache(),
+});
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ApolloProvider client={client}>
+//       <FiltersProvider>
+//         <Provider store={store}>
+//         <App />
+//         </Provider>
+//       </FiltersProvider>
+//     </ApolloProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <FiltersProvider>
+        <Provider store={store}>
+        <App />
+        </Provider>
+      </FiltersProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
